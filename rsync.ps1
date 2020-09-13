8d819647abd29bbb09a39c79cb789c93d1b75abf
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-13 15:57:45 +0900

    update rsync.ps1

--- rsync.ps1
+++ rsync.ps1
@@ -1,8 +1,18 @@
 Param(
-    [parameter(mandatory=$true)]$from,
-    [parameter(mandatory=$true)]$to,
-    [parameter(mandatory=$true)]$option
+    [string]$from,
+    [string]$to,
+    [string]$single_options,
+    [string]$double_options,
+    [string]$exclude_paths,
+    [switch]$help
 )
+
+if($help) {
+    Write-Output("this command is run rsync from wsl2.")
+    Write-Output('example: rsync.ps1 -single_options "av" -exclude_paths "node_modules" -from "C:\Downloads" -to "D:\bak"')
+    Write-Output('when use multi parameter, use ":".\n example: -single_options "archive:verbose"')
+    return
+}
 
 $fromEscaped = [regex]::escape($from)
 $toEscaped = [regex]::escape($to)
@@ -10,4 +20,31 @@ $toEscaped = [regex]::escape($to)
 $fromDecodePath = wsl wslpath $fromEscaped
 $toDecodePath = wsl wslpath $toEscaped
 
-wsl rsync $option $fromDecodePath $toDecodePath --exclude=node_modules
+$singleOptionList = ""
+if($single_options -ne "") {
+    foreach($i in $single_options.split(":")) {
+        $singleOptionList = $singleOptionList + "-" + $i + " "
+    }
+}
+
+$doubleOptionList = ""
+if($double_options -ne "") {
+    foreach($i in $double_options.split(":")) {
+        $doubleOptionList = $doubleOptionList + "--" + $i + " "
+    }
+}
+
+$excludePathList = ""
+if($exclude_paths -ne "") {
+    foreach($i in $exclude_paths.split(":")) {
+        $excludePathList = $excludePathList + "--exclude=" + $i + " "
+    }
+}
+
+$final = "wsl rsync " + $singleOptionList + $doubleOptionList + $fromDecodePath + " " + $toDecodePath + " " + $excludePathList
+
+Write-Output("run " + $final)
+
+Invoke-Expression($final)
+
+Write-Output("done!")
