9d4fea5d949eab7795adb7aadd52568370fd0cc9
madosuki <yurikasamaaikatsu@gmail.com>
2020-12-07 14:34:40 +0900

    add rmall.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,3 +2,5 @@ Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteChar
 Set-PSReadlineKeyHandler -Chord Tab -Function MenuComplete
+
+Set-Alias -name rmall -value Remove-Item -Recurse
