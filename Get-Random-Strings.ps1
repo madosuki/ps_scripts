916f02e6b93c84d2d946d1d870b81b27a87e3a9f
madosuki <yurikasamaaikatsu@gmail.com>
2021-10-13 20:18:18 +0900

    update Get-Random-Strings.ps1

--- Get-Random-Strings.ps1
+++ Get-Random-Strings.ps1
@@ -1,7 +1,23 @@
-Param($num)
+Param([int32]$num, [switch]$enable_symbol)
 if (!$num) {
 	Write-Output "require a argument of string size number"
 	return
 }
 
--join ((48..57) + (65..90) + (97..122) | Get-Random -count $num | ForEach-Object { [char]$_} )
+function GetCollection {
+	if ($enable_symbol) {
+		return 33..122
+	}
+	
+	return (48..57) + (65..90) + (97..122) 
+}
+
+$collection = GetCollection
+
+$result = New-Object System.Collections.Generic.List[System.Object]
+for ($i = 0; $i -lt $num; $i++) {
+	$tmp = [char]($collection | Get-Random)
+	$result.Add($tmp)
+}
+
+-join $result
