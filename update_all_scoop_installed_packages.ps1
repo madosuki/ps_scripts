a3b947742612b580c88534c2e1197bbaf6791adb
madosuki <yurikasamaaikatsu@gmail.com>
2022-12-07 18:16:00 +0900

    update

--- update_all_scoop_installed_packages.ps1
+++ update_all_scoop_installed_packages.ps1
@@ -1,1 +1,10 @@
+where.exe scoop /Q
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Output "not found scoop from path"
+    return
+}
+
+
+
 scoop update && scoop status | Select-Object -Property Name | ForEach-Object { scoop update $_.Name }
