7f733d2c9967f9f10b92768c8fef86cac00f3dcf
madosuki <yurikasamaaikatsu@gmail.com>
2023-04-25 22:07:24 +0900

    update

--- run_timer.ps1
+++ run_timer.ps1
@@ -1,3 +1,11 @@
-# timer.exe is generate by `cargo build` on https://github.com/madosuki/timer-r
+# timer_with_voice.exe is generate by `cargo build` on https://github.com/madosuki/timer-r
 
-.\bin\timer.exe -f .\voice.wav $args
+$get_command_result = $(where.exe timer_with_voice)
+
+if (Test-Path($get_command_result)) {
+    timer_with_voice.exe -f .\voice.wav $args
+} else {
+    Write-Output "not found timer_with_voice.exe"
+}
+
+
